5f5e0fd348c6b0fc2fbe165f7019a5fd88dc008d
Mattias Aabmets <mattias.aabmets@gmail.com>
2023-11-12 18:48:49 +0200

    Removed unnecessary docstrings which are later moved into stub file

--- kupydo/inner/client.py
+++ kupydo/inner/client.py
@@ -20,15 +20,6 @@ __all__ = ["ApiClient"]
 
 class ApiClient:
     def __init__(self, *, autoconfig: bool = True) -> None:
-        """
-        :param autoconfig: When set to True, Kupydo will first try to load the
-            in-cluster config. If unsuccessful, it tries to load the current
-            context config. When set to False, the user is responsible for
-            loading the correct custom config using the `kupydo.config` module.
-        :raises RuntimeError: Only on `'async with'`, if Kupydo has not been
-            configured manually or is unable to automatically configure itself
-            when autoconfig is set to True.
-        """
         self._autoconfig = autoconfig
 
     async def __aenter__(self):
@@ -42,35 +33,20 @@ class ApiClient:
 
     @error_handler
     async def create(self, model: KupydoBaseModel) -> Response[RawModel]:
-        """
-        :raises None:
-        """
         return await model.create(self._client)
 
     @error_handler
     async def delete(self, model: KupydoBaseModel) -> Response[RawModel]:
-        """
-        :raises None:
-        """
         return await model.delete(self._client)
 
     @error_handler
     async def read(self, model: KupydoBaseModel) -> Response[RawModel]:
-        """
-        :raises None:
-        """
         return await model.read(self._client)
 
     @error_handler
     async def replace(self, model: KupydoBaseModel, **kwargs) -> Response[RawModel]:
-        """
-        :raises pydantic.ValidationError:
-        """
         return await model.replace(self._client, kwargs)
 
     @error_handler
     async def patch(self, model: KupydoBaseModel, **kwargs) -> Response[RawModel]:
-        """
-        :raises pydantic.ValidationError:
-        """
         return await model.patch(self._client, kwargs)
